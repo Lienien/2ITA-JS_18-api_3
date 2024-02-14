d95055e256498c30a215e9fd51fb9df49878823c
Lienien <116272320+Lienien@users.noreply.github.com>
2024-02-14 13:40:42 +0100

    bing bada boom

---
+++ script.js
@@ -0,0 +1,47 @@
+const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://hp-api.onrender.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p id="House">House: ${character.house}</p>
+                <p id="Gender">Gender: ${character.gender}</p>
+                <p id="Blood">Blood status: ${character.ancestry}</p>
+                <p id="Patronus">Patronus: ${character.patronus}</p>
+                <p id="year">Year of birth: ${character.yearOfBirth}</p>
+                <p id="actor">Actor: ${character.actor}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
